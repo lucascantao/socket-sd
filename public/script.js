92239f9ee637ae89d0434669a59bbaa79421d38e
lucascantao <cantao162@gmail.com>
2024-06-15 21:41:37 -0300

    sei la o que foi isso

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-const botaoEnviar = document.getElementById('enviar');
-const caixaDeMensagem = document.getElementById('texto');
-const chat = document.getElementById('mensagens');
+// const botaoEnviar = document.getElementById('enviar');
+// const caixaDeMensagem = document.getElementById('texto');
+// const chat = document.getElementById('mensagens');
 
 const socket = io('localhost:3000');
 
@@ -8,9 +8,17 @@ socket.on('connect', () => {
     $('#myId span').text(socket.id);
 });
 
-socket.addEventListener('regist', (regist) => {
-    $('.id').text(regist.id);
-    $('.state').text(regist.state);
+socket.addEventListener('objects', (objects) => {
+    $('#objects').empty();
+    objects.forEach(obj => {
+        $('#objects').append(
+            `<div class="object-item">
+            <img src="./assets/icons/${obj.icon}.png"></img>
+            <i class="id">${obj.id}</i>
+            <i class="socket">${obj.socket}</i>
+            </div>`
+        )
+    })
 })
 
 socket.addEventListener('update connected', (connected) => {
@@ -19,3 +27,15 @@ socket.addEventListener('update connected', (connected) => {
         $('#connected').append(`<div class="client-item"> <div class="client-item-id">${i}</div> <div class="client-item-value" >${c}</div> </div>`);
     });
 });
+
+
+$('#bind').on('click', () =>{
+    const id = $('#objectId').val();
+    const icon = $('#icon').val();
+    socket.emit('bind', (
+        {
+            id: id,
+            icon: icon
+        }
+    ));
+})
--- servidor.js
+++ servidor.js
@@ -10,27 +10,71 @@ const io = require('socket.io')(servidorHttp, {
     }
 });
 
-const regist = {
-    id: 'door',
-    state: 'open'
-}
+const objects = [
+    {
+        id: 'door',
+        socket: '',
+        icon: 'Stelle'
+    }
+]
 
 let connected = []
 
+// ao se conectar no servidor
 io.addListener('connection', (socket) => {
     console.log('Um usuário conectou.')
 
+    // atualizar lista de conectados no back
     const client_id = socket.id;
-
     connected.push(client_id);
     
-    io.emit('regist', regist);
-
+    // atualizar a lista de conectados no client
     io.emit('update connected', connected);
 
+    // atualizar a lista de objetos no client
+    io.emit('objects', objects);
+
+    // ao se desconectar
     socket.conn.on('close', (reason) => {
+        
+        // remover da lista de cientes
         connected = connected.filter((id) => id !== client_id);
+        
+        // remover da lista de objetos
+        objects.forEach(obj => {
+            if(obj.socket === socket.id) {
+                obj.socket = '';
+            }
+        });
+
+        // atualizar informações no front
+        io.emit('objects', objects);
         io.emit('update connected', connected);
+    });
+
+    
+    socket.addListener('bind', (bindObject) => {
+
+        const object = objects.find( obj => obj.id === bindObject.id );
+
+        console.log(object)
+
+        if(object !== undefined) {
+            object.socket = socket.id;
+            object.icon = bindObject.icon
+            console.log(`bind ${socket.id} to ${bindObject.id}`);      
+        } else  {
+            objects.push(
+                {
+                    id: bindObject.id,
+                    socket: socket.id,
+                    icon: bindObject.icon
+                }
+            );
+            console.log(`bind ${socket.id} to ${bindObject.id}`);
+        }
+        
+        io.emit('objects', objects);
     });
 
     // socket.addListener('nova mensagem', (msg) => {
